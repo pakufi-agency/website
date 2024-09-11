454a584b61c5fd2b0c3fc886d6e2d6c2cda040f7
gio <designbygio@pm.me>
2024-09-11 11:38:45 +0200

    clean up scss an unused themes, change font t roboto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ComingSoonContent from "../components/ComingSoon/ComingSoonContent";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Pakufi - Ethical Tech Agency",
+  description:
+    "We help you bring your ideas online pofessionally and tailored to you. We work just with talente freelancers from less priviledge countries, offering opportunity to achieve economical and geographical freedom.",
+};
 
 export default async function Page() {
   return (
