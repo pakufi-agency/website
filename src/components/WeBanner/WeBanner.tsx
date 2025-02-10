9bab86e7ddbe56ac228e23e406d7b8ec30ef77ee
gio <designbygio@pm.me>
2025-02-10 14:51:11 +0100

    finilize hero, we statment

--- src/components/WeBanner/WeBanner.tsx
+++ src/components/WeBanner/WeBanner.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 
 import styles from "./WeBanner.module.scss";
 
-import ctaShape from "/public/images/legacy/cta-shape.png";
 import circle from "/public/images/backgrounds/green-spiral-bg.svg";
 
 interface WeBannerProps {
@@ -24,21 +23,20 @@ const WeBanner: React.FC<WeBannerProps> = ({
       <div className={`${styles.ctaArea}`}>
         <div className="container">
           <div className="row align-items-center">
-            <div className="col-lg-10">
+            <div className="col-lg-4">
+              <div className={`${styles.ctaShape}`}>
+                <Image src={circle} alt="image" width={821} height={801} />
+              </div>
+            </div>
+            <div className="col-lg-8">
               <div className={styles.ctaContent}>
+                <h1>Empowering freedom, Bridging gaps in global inequality</h1>
                 <h2>{statmentOne}</h2>
-                <h2>{statmentTwo}</h2>
                 <h2>{statmentThree}</h2>
+                <h2>{statmentTwo}</h2>
               </div>
             </div>
           </div>
-        </div>
-
-        {/* <div className={styles.circleBox}>
-          <Image src={circle} alt="image" width={256} height={507} />
-        </div> */}
-        <div className={`${styles.ctaShape}`}>
-          <Image src={circle} alt="image" width={521} height={501} />
         </div>
       </div>
     </>
