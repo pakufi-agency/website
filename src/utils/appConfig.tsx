491ccc96ad9cde5608560cb6eee6ff4c21fa469d
designbygio <gsambrotta@users.noreply.github.com>
2025-10-23 18:45:39 +0200

    Add Global Banner & App Feature Config (#38)
    - create appConfig file where we can toggle features
    - create GlobalBanner component
    - Add GlobalBanner to Nav
    - toggle GlobalBanner and Nav padding based on banner visibility

---
+++ src/utils/appConfig.tsx
@@ -0,0 +1,7 @@
+export const appConfig = {
+  features: {
+    mentorshipBanner: true,
+  },
+  mentorshipBannerText: "Pakufi mentorship program now open!",
+  mentorshipBannerLink: "/talent-growth",
+};
