07b9a3f2cf378bef6b0bf5e39c6bcf61cba10e3d
designbygio <designbygio@pm.me>
2025-10-29 18:32:38 +0100

    add sitemap for SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: "https://pakufi.agency",
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: "weekly",
+  priority: 0.7,
+};
