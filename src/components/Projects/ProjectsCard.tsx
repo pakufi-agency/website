6eb2b4e67077a37abb07b27e4b38485e497adfba
designbygio <designbygio@pm.me>
2026-06-25 21:22:18 +0200

    fix conatainer fit in Projects Card

--- src/components/Projects/ProjectsCard.tsx
+++ src/components/Projects/ProjectsCard.tsx
@@ -28,7 +28,7 @@ const ProjectsCard: React.FC<ProjectsListProps> = ({ items = [] }) => {
                       className={styles.projectImage}
                       sizes="(min-width: 992px) 33vw, (min-width: 768px) 50vw, 100vw"
                       fill
-                      objectFit="container"
+                      objectFit="contain"
                     />
 
                     <span className={styles.icon}>
