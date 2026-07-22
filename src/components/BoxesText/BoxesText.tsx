096db0b67b5c5a2272aa7deaef1b14dcccae00f8
henribraho12-maker <henribraho12@gmail.com>
2026-07-22 16:50:21 +0200

    CSS styling of boxesText part of Who is it for section in talent growth page

--- src/components/BoxesText/BoxesText.tsx
+++ src/components/BoxesText/BoxesText.tsx
@@ -18,7 +18,10 @@ const BoxesText: React.FC<BoxesTextProps> = ({ items }) => {
   return (
     <div className={`${styles.container}`}>
       {items.map((item, index) => (
-        <div key={index} className={styles.box}>
+        <div
+          key={index}
+          className={`${styles.box} ${index === 0 ? styles.positive : styles.negative}`}
+        >
           <div className={styles.text}>
             <BlockRendererClient content={item.content} />
           </div>
