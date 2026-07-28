cbebdcd33194b8b44e152edad12fe3b9cb70a049
henribraho12-maker <henribraho12@gmail.com>
2026-07-28 12:42:08 +0200

    Imported Reviews Component In Talent Growth Page And Also changed the query of Talent Growth Page in able for Client Reviews To Be Shown On the page

--- src/graphqlQueries/TalentGrowth.tsx
+++ src/graphqlQueries/TalentGrowth.tsx
@@ -114,6 +114,12 @@ export const TALENTGROWTH_QUERY = gql`
           TextBlock {
             content
           }
+          client_reviews {
+          name
+          reviewTitle
+          reviewContent
+          rating
+          }
         }
         ... on ComponentCommonSectionhalfbackground {
           sectionTitle
